83e4388a4e60a45dc44e0a34778670ffcc99fd6d
S1NH <hbyls@live.com>
2023-04-11 15:24:06 +0800

    Site updated: 2023-04-11 15:24:05

--- js/third-party/analytics/matomo.js
+++ js/third-party/analytics/matomo.js
@@ -1,19 +1,18 @@
 /* global CONFIG */
 
 if (CONFIG.matomo.enable) {
-  window._paq = window._paq || [];
-  const _paq = window._paq;
-
+  var _paq = window._paq = window._paq || [];
   /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+  _paq.push(["setCookieDomain", "*.s1nh.org"]);
+  _paq.push(["setDomains", ["*.s1nh.org","*.s1nh.com"]]);
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
-  const u = CONFIG.matomo.server_url;
-  _paq.push(['setTrackerUrl', u + 'matomo.php']);
-  _paq.push(['setSiteId', CONFIG.matomo.site_id]);
-  const d = document;
-  const g = d.createElement('script');
-  const s = d.getElementsByTagName('script')[0];
-  g.async = true;
-  g.src = u + 'matomo.js';
-  s.parentNode.insertBefore(g, s);
+  (function() {
+    var u="//home.s1nh.com:32443/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
 }
