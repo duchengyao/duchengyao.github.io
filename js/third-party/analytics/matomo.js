beeb0debd3d76bc2c92e33e0868afa642d242f55
S1NH <hbyls@live.com>
2023-04-18 15:33:05 +0800

    Site updated: 2023-04-18 15:33:04

--- js/third-party/analytics/matomo.js
+++ js/third-party/analytics/matomo.js
@@ -3,7 +3,7 @@
 if (CONFIG.matomo.enable) {
   var _paq = window._paq = window._paq || [];
   /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-  _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+  _paq.push(["setDocumentTitle", document.title + " | " + document.domain]);
   _paq.push(["setCookieDomain", "*.s1nh.org"]);
   _paq.push(["setDomains", ["*.s1nh.org","*.s1nh.com"]]);
   _paq.push(['trackPageView']);
@@ -12,6 +12,8 @@ if (CONFIG.matomo.enable) {
     var u="https://home.s1nh.com:32443/";
     _paq.push(['setTrackerUrl', u+'matomo.php']);
     _paq.push(['setSiteId', '1']);
+    _paq.push(['addTracker', u+'matomo.php', 2]);
+    _paq.push(['addTracker', u+'matomo.php', 3]);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
   })();
