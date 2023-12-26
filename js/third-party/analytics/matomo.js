0d69b3da962d1bc072926a6a8c00869f6f5b8946
S1NH <hbyls@live.com>
2023-12-26 16:29:20 +0800

    Site updated: 2023-12-26 16:29:19

--- js/third-party/analytics/matomo.js
+++ js/third-party/analytics/matomo.js
@@ -1,31 +1,19 @@
 /* global CONFIG */
 
 if (CONFIG.matomo.enable) {
-  var _paq = window._paq = window._paq || [];
+  window._paq = window._paq || [];
+  const _paq = window._paq;
+
   /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-  if (document.domain.indexOf('com') != -1){
-    _paq.push(["setCookieDomain", "*.s1nh.com"]);
-  } else if(document.domain.indexOf('org') != -1){
-    _paq.push(["setCookieDomain", "*.s1nh.org"]);
-  }
-  _paq.push(["setDomains", ["*.s1nh.org","*.s1nh.com"]]);
-  _paq.push(["enableCrossDomainLinking"]);
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="https://matomo.s1nh.org/";
-    _paq.push(['setTrackerUrl', u+'matomo.php']);
-    
-    if (document.domain.indexOf('com') != -1){
-      _paq.push(['setSiteId', '2']);
-    } else if(document.domain.indexOf('org') != -1){
-      _paq.push(['setSiteId', '3']);
-    }
-    
-    // Will also collect the website data into Website ID = 1
-    _paq.push(['addTracker', u+'matomo.php', 1]);
-    
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-  })();
+  const u = CONFIG.matomo.server_url;
+  _paq.push(['setTrackerUrl', u + 'matomo.php']);
+  _paq.push(['setSiteId', CONFIG.matomo.site_id]);
+  const d = document;
+  const g = d.createElement('script');
+  const s = d.getElementsByTagName('script')[0];
+  g.async = true;
+  g.src = u + 'matomo.js';
+  s.parentNode.insertBefore(g, s);
 }
